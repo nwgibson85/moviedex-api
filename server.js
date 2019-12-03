2f731742efd21ecff4f7455c9a01cd2b3cac927d
Nicholas Gibson <Nwgibson85@gmail.com>
2019-12-03 10:07:55 -0600

    removed console.logs

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  console.log('1', authToken, apiToken);
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
@@ -29,7 +28,6 @@ app.get('/movie', function handleGetMovie(req, res) {
   let response = MOVIELIST.movies;
   
   if (req.query.country) {
-    console.log('country', req.query.country)
     response = response.filter(movies =>
       // case insensitive searching
       movies.country.toLowerCase().includes(req.query.country.toLowerCase())
@@ -37,20 +35,20 @@ app.get('/movie', function handleGetMovie(req, res) {
   }
 
   if (req.query.genre) {
-    console.log('genre', req.query.genre);
     response = response.filter(movies =>
       //case sensitive searching
-      movies.genre.includes(req.query.genre)
+      movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     )
   }
 
   if (req.query.avg_vote) {
-    console.log('Avg_Vote', req.query.avg_vote);
-    // return avg_vote >= 
+    // return avg_vote >= req.query.avg_vote
     response = response.filter(movies =>
       movies.avg_vote >= Number(req.query.avg_vote)
     )
   }
+
+  res.json(response)
 });
 
 const PORT = 8000
